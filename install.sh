529c12d705efb5014c8239c213a45fe7042f3e2f
tatara42 <murasakitatar@gmail.com>
2025-12-04 14:03:22 +0000

    remove terminal emulator config

---
+++ install.sh
@@ -0,0 +1,3 @@
+sudo apt install neovim zsh fzf git gh tmux stow unzip
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
